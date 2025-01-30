a06aba0c6fddb54a6513b7eb5e8e5545ccd34da4
Abnv06 <you@example.com>
2025-01-30 10:55:11 +0530

    Initial commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import axios from 'axios'
+import { Baseurl } from './baseUrl'
+import Loader from './Loader'
+import './Exchanges.css'
+import OurModel from './OurModel'
+
+const Exchanges = () => {
+
+  const [loading, setLoading] = useState(true)
+  const [exchanges, setExchanges] = useState([])
+
+  useEffect(() => {
+    const getExchangesData = async () => {
+      const { data } = await axios.get(`${Baseurl}/exchanges`)
+      console.log(data)
+      setExchanges(data)
+      setLoading(false)
+    }
+    getExchangesData()
+  }, [])
+
+
+  return (
+
+    <>
+      {
+        loading ? <Loader /> : <>
+          <Header />
+          <OurModel />
+          <div>
+            {
+              exchanges.map((item, i) => {
+                return (
+                  <div key={i} className='ex-cards'>
+                    <div className='image'>
+                      <img height={"80px"} src={item.image} alt="" />
+                    </div>
+                    <div className="name">
+                      {item.name}
+                    </div>
+                    <div className="price">
+                      {item.trade_volume_24h_btc.toFixed()}
+                    </div>
+                    <div className="rank">
+                      {item.trust_score_rank}
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+        </>
+
+      }
+    </>
+  )
+
+}
+
+export default Exchanges
+
+
+
+
+
+// <div className='ex-cards'>
+//               <div className='image'>
+//                 <img height={"80px"} src={coin} alt="" />
+//               </div>
+//               <div className="name">
+//                 Ethereum
+//               </div>
+//               <div className="price">
+//                 120000$
+//               </div>
+//               <div className="rank">
+//                 2
+//               </div>
+//             </div>
